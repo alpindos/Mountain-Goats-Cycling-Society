cacfc00c3d54e544a822da3e219fc772d0997a33
alpindos <75666368+alpindos@users.noreply.github.com>
2021-08-23 13:34:13 +0300

    Add files via upload

--- js/mgScript.js
+++ js/mgScript.js
@@ -1,1 +1,12 @@
+const routeOne = document.getElementById("routeOne");
+const routeTwo = document.getElementById("routeTwo");
 
+const buttonOne = document.getElementById("buttonOne");
+const buttonTwo = document.getElementById("buttonTwo");
+
+buttonOne.addEventListener("click", function() { toggle(routeOne); });
+buttonTwo.addEventListener("click", function() { toggle(routeTwo); });
+
+function toggle(e) {
+	e.style.display = (e.style.display === "none") ? "block" : "none" ;
+}
